242bd2188320c5456438d376d854e988eaf39c62
student <student@B526-17.fesb.hr>
2016-03-08 08:41:31 +0100

    first commit

---
+++ visual/visual/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
